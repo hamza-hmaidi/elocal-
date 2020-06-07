673be467ae60846d66f9188e368aedfa0436662c
Hamzahmaidi <hamza.hmaidi@insat.u-carthage.tn>
2020-06-08 00:02:01 +0100

    add modal for sign up form

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
-var loginbtn = document.querySelector(".navbtn");
-var modal = document.getElementById("login-modal");
-var body = document.querySelector("body"); 
-var closebtn =document.querySelector(".modal .cancel");
-function openForm() {
-  modal.style.display = "block";
+
+var navbtn = document.querySelectorAll(".navbtn");
+var loginbtn=navbtn[0];
+var signupbtn=navbtn[1]; 
+var closebtns =document.querySelectorAll(".modal .cancel");
+var closebtnlogin=closebtns[0]; 
+var closebtnsignup=closebtns[1]; 
+//sign in form : 
+var signupmodal = document.getElementById("signup-modal");
+
+function openSignupForm() {
+  signupmodal.style.display = "block";
   //disable body scrolling when form is open
   body.style.overflow = "hidden" ; 
-
 }
-loginbtn.addEventListener("click",openForm);
+signupbtn.addEventListener("click",openSignupForm);
+closebtnsignup.addEventListener("click",closeForm);
+// login form : 
+
+var loginmodal = document.getElementById("login-modal");
+var body = document.querySelector("body"); 
+var closebtnlogin =document.querySelector(".modal .cancel");
+function openLoginForm() {
+  loginmodal.style.display = "block";
+  //disable body scrolling when form is open
+  body.style.overflow = "hidden" ; 
+}
+loginbtn.addEventListener("click",openLoginForm);
 
 
 
-// When the user clicks anywhere outside of the modal, close it
-
+// When the user clicks anywhere outside of the loginmodal, close it
 
 function closeForm(){
-		modal.style.display = "none";
-	    //enable body scrolling when form is open
+		loginmodal.style.display = "none";
+		signupmodal.style.display = "none";
+	    //enable body scrolling when form is closed
 	    body.style.overflow = "auto" ;
 }
-closebtn.addEventListener("click", closeForm);
+closebtnlogin.addEventListener("click", closeForm);
 window.addEventListener("click",function(event){
-	if (event.target == modal) {
-	    modal.style.display = "none";
-	    //enable body scrolling when form is open
+	if (event.target == loginmodal || event.target == signupmodal ) {
+	    loginmodal.style.display = "none";
+	    signupmodal.style.display = "none";
+	    //enable body scrolling when form is closed
 	    body.style.overflow = "auto" ;
 	  }
 })
